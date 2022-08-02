f1fa1fa7e30ab236fb3ad14bdd8facd03a2af9c2
daniel-chang-github <81652137+daniel-chang-github@users.noreply.github.com>
2022-08-01 23:52:34 -0400

    Add files via upload

--- Step 2 Data Ingestion/data_ingestion.py
+++ Step 2 Data Ingestion/data_ingestion.py
@@ -1,54 +1,52 @@
 #%%
 from distutils.command.config import config
-from typing import List
-import decimal
 import json
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType,StructField,StringType,IntegerType,DecimalType
+from pyspark.sql.types import *
 import config
+from datetime import datetime
 
 #%%
 #Parse csv fuction
 def parse_csv(line:str):
+    record_type_pos = 2
+    record = line.split(",")
     try:    
-        record_type_pos = 2
-        record = line.split(",")
-        
         # [logic to parse records]
-
-        if record[record_type_pos] == "T":            
-            event = [record[0],\
-                     record[1],\
-                     record[2],\
-                    #  record[2],\
-                     record[3],\
-                     record[4],\
-                     int(record[5]),\
-                     record[6],\
-                     decimal.Decimal(record[7]),\
-                     int(record[8]),\
-                     decimal.Decimal(record[9]),\
-                     int(record[10]),\
-                     "T"]
+        if record[record_type_pos] == 'T':
+            event = (datetime.strptime(record[0], '%Y-%m-%d').date(),
+                      record[2],
+                      record[3],
+                      record[6],
+                      datetime.strptime(record[4], '%Y-%m-%d %H:%M:%S.%f'),
+                      int(record[5]),
+                      datetime.strptime(record[1], '%Y-%m-%d %H:%M:%S.%f'),
+                      float(record[7]),
+                      None,
+                      None,
+                      None,
+                      None,
+                      record[2])
             return event
-        elif record[record_type_pos] == "Q":
-            event = [record[0],\
-                     record[1],\
-                     record[2],\
-                     record[3],\
-                     record[4],\
-                     int(record[5]),\
-                     record[6],\
-                     decimal.Decimal(record[7]),\
-                     int(record[8]),\
-                     decimal.Decimal(record[9]),\
-                     int(record[10]),\
-                     "Q"]
+        elif record[record_type_pos] == 'Q':
+            event = (datetime.strptime(record[0], '%Y-%m-%d').date(),
+                    record[2],
+                    record[3],
+                    record[6],
+                    datetime.strptime(record[4], '%Y-%m-%d %H:%M:%S.%f'),
+                    int(record[5]),
+                    datetime.strptime(record[1], '%Y-%m-%d %H:%M:%S.%f'),
+                    None,
+                    float(record[7]),
+                    int(record[8]),
+                    float(record[9]),
+                    int(record[10]),
+                    record[2])
             
             return event
     except Exception as e:
-        event = [None,None,None,None,None,None,None,None,None,None,None,"B"]
-        return event
+        event = [None,None,None,None,None,None,None,None,None,None,None,None,"B"]
+        return event    
 
 #%%
 #Parse json fuction
@@ -59,56 +57,57 @@ def parse_json(line:str):
                                             
         # [logic to parse records]
         if record_type == "T":            
-            event = [record['trade_dt'],\
-                     record['file_tm'],\
-                     record['event_type'],\
-                     record['symbol'],\
-                     record['event_tm'],\
-                     int(record['event_seq_nb']),\
-                     record['exchange'],\
-                     decimal.Decimal(record['bid_pr']),\
-                     int(record['bid_size']),\
-                     decimal.Decimal(record['ask_pr']),\
-                     int(record['ask_size']),\
-                     "T"]
+            event = (datetime.strptime(line['trade_dt'], '%Y-%m-%d').date(),
+                    record_type,
+                    line['symbol'],
+                    line['exchange'],
+                    datetime.strptime(line['event_tm'], '%Y-%m-%d %H:%M:%S.%f'),
+                    line['event_seq_nb'],
+                    datetime.strptime(line['file_tm'], '%Y-%m-%d %H:%M:%S.%f'),
+                    line['price'],
+                    None,
+                    None,
+                    None,
+                    None,
+                    record_type)
             return event
         elif record_type == "Q":
-            event = [record['trade_dt'],\
-                     record['file_tm'],\
-                     record['event_type'],\
-                     record['symbol'],\
-                     record['event_tm'],\
-                     int(record['event_seq_nb']),\
-                     record['exchange'],\
-                     decimal.Decimal(record['bid_pr']),\
-                     int(record['bid_size']),\
-                     decimal.Decimal(record['ask_pr']),\
-                     int(record['ask_size']),\
-                     "Q"]
+            event = (datetime.strptime(line['trade_dt'], '%Y-%m-%d').date(),
+                    record_type,
+                    line['symbol'],
+                    line['exchange'],
+                    datetime.strptime(line['event_tm'], '%Y-%m-%d %H:%M:%S.%f'),
+                    line['event_seq_nb'],
+                    datetime.strptime(line['file_tm'], '%Y-%m-%d %H:%M:%S.%f'),
+                    None,
+                    line['bid_pr'],
+                    line['bid_size'],
+                    line['ask_pr'],
+                    line['ask_size'],
+                    record_type)
             return event
     except Exception as e:
-        event = [None,None,None,None,None,None,None,None,None,None,None,"B"]
+
+        event = [None,None,None,None,None,None,None,None,None,None,None,None,"B"]
         return event
 
 #%%
 #data from two different sources will be put into this schema.
-common_event = StructType([\
-                    StructField("trade_dt", StringType(),True),\
-                    StructField("arrival_tm", StringType(),True),\
-                    StructField("rec_type", StringType(),True),\
-                    StructField("symbol", StringType(),True),\
-                    StructField("event_tm", StringType(),True),\
-                    StructField("event_seq_nb", IntegerType(),True),\
-                    StructField("trade_pr", StringType(),True),\
-                    StructField("bid_pr", DecimalType(),True),\
-                    StructField("bid_size", IntegerType(),True),\
-                    StructField("ask_pr", DecimalType(),True),\
-                    StructField("ask_size", IntegerType(),True),\
-                    StructField("partition", StringType(),True)\
-                          ])
-
+common_event = StructType([StructField('trade_dt', DateType(), True),
+                           StructField('rec_type', StringType(), True),
+                           StructField('symbol', StringType(), True),
+                           StructField('exchange', StringType(), True),
+                           StructField('event_tm', TimestampType(), True),
+                           StructField('event_seq_nb', IntegerType(), True),
+                           StructField('arrival_tm', TimestampType(), True),
+                           StructField('trade_pr', FloatType(), True),
+                           StructField('bid_pr', FloatType(), True),
+                           StructField('bid_size', IntegerType(), True),
+                           StructField('ask_pr', FloatType(), True),
+                           StructField('ask_size', IntegerType(), True),
+                           StructField('partition', StringType(), True)])
 #%%
-storage_account_name =config.storage_account_access_key
+storage_account_name =config.storage_account_name
 storage_account_access_key = config.storage_account_access_key
 container_name = config.container_name
 
@@ -120,31 +119,8 @@ UTIL = '/Users/daniel/Desktop/Data_Engineering/Guided Capstone/jars/jetty-util-9
 
 #%%
 from pyspark.sql import SparkSession
-# spark = SparkSession.builder.master('local').appName('app').getOrCreate()
 spark = SparkSession.builder.config('spark.jars', f"{AZURE_STORAGE_JAR_PATH},{HADOOP_AZURE_JAR_PATH},{JSON_CON},{UTIL}").getOrCreate()
 spark.conf.set("fs.azure.account.key.{}.blob.core.windows.net".format(storage_account_name),"{}".format( storage_account_access_key))
-
-dates = ['2020-08-05','2020-08-06']
-csvlist = []
-jsonlist = []
-
-for dt in dates:
-    rawcsv = spark.sparkContext.textFile("wasbs://{}@{}.blob.core.windows.net/{}".format(container_name,storage_account_name, 'data/csv/{}/NYSE/*.txt'.format(dt)))
-    rawjson = spark.sparkContext.textFile("wasbs://{}@{}.blob.core.windows.net/{}".format(container_name,storage_account_name, 'data/json/{}/NASDAQ/*.txt'.format(dt)))
-    
-    parsed_csv = rawcsv.map(lambda line: parse_csv(line))
-    parsed_json = rawjson.map(lambda line: parse_json(line))
-
-    datacsv = spark.createDataFrame(parsed_csv, common_event)
-    datajson = spark.createDataFrame(parsed_json, common_event)
-
-    csvlist.append(datacsv)
-    jsonlist.append(parsed_json)
-
-
-
-#%%
-print(csvlist)
 
 
 #%% 
@@ -152,7 +128,7 @@ dates = ['2020-08-05','2020-08-06']
 csvlist = []
 jsonlist = []
 
-spark = SparkSession.builder.getOrCreate()
+# spark = SparkSession.builder.getOrCreate()
 for dt in dates:
     rawcsv = spark.sparkContext.textFile("wasbs://{}@{}.blob.core.windows.net/{}".format(container_name,storage_account_name, 'data/csv/{}/NYSE/*.txt'.format(dt)))
     rawjson = spark.sparkContext.textFile("wasbs://{}@{}.blob.core.windows.net/{}".format(container_name,storage_account_name, 'data/json/{}/NASDAQ/*.txt'.format(dt)))
@@ -169,8 +145,10 @@ for dt in dates:
 
 csv_data = csvlist[0].union(csvlist[1])
 json_data= jsonlist[0].union(jsonlist[1])
+
+
 all_data = csv_data.union(json_data)
-all_data.show(800)
+all_data.show()
 
 all_data.write.partitionBy("partition").mode("overwrite").parquet("/Users/daniel/Desktop/Data_Engineering/Guided Capstone/data/output")
 
